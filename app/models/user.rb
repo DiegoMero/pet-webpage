66e258c5b60b088048c9442cac5e62ae6438c1d3
DiegoMero <diegomero_1999@hotmail.com>
2024-03-29 10:09:38 -0500

    Add devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :donations
   has_many :sponsorships
+
+  before_create :set_default_petCoins
+
+  private
+
+  def set_default_petCoins
+    self.petCoins ||= 1000
+  end
 end
